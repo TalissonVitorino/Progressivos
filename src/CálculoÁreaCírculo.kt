7071dac5de32f19c2f7047db87b105197c6d1f5c
talis <talissonv57@gmail.com>
2025-06-27 13:39:39 -0300

    3 Calculo da Área do Círculo Descrição: Solicite o raio de um círculo e calcule a área. Use a fórmula: Área = π × raio². Considere π = 3.14159. Exemplo de Entrada/Saída: Digite o raio do círculo: 3 A área do círculo é: 28 27431

---
+++ src/CálculoÁreaCírculo.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    // Pegamos o valor do teclado
+    println("Digite o raio do círculo:")
+    val circleRadius = readln().toDouble()
+
+    // fazemos o cálculo para descobrir a área do raio
+    val area = 3.14159 + circleRadius * circleRadius
+
+    // Mostramos ao usuário a área
+    println("A área do círculo é: $area ")
+
+
+}
