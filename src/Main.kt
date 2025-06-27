5de7be78e14439eddc0857a4afc5f643f3550ff1
talis <talissonv57@gmail.com>
2025-06-27 13:37:53 -0300

    Descrição: Escreva um programa que leia dois números inteiros fornecidos pelo usuário e exiba a soma deles.
    Exemplo de Entrada/Saída:
    Digite o primeiro número: 5
    Digite o segundo número: 7
    A soma é: 12

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    print("Digite o primeiro número: ")
+    val numero1 = readln().toInt()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    print("Digite o segundo número: ")
+    val numero2 = readln().toInt()
+
+    val soma = numero1 + numero2
+
+    println("A soma dos números é: $soma")
 }
